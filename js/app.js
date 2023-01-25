fb168f728b566d56701d984053ae3a5f254dbf8a
Ignacio Velazquez <ignaciov92@gmail.com>
2023-01-24 23:38:25 -0300

    logica terminada

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-const userLog = JSON.parse(localStorage.getItem("userLog"))
-console.log(userLog)
+if (!localStorage.getItem("userLog")) {
+  window.location= "../pages/login.html";
+}
+
+const closeSesion= ()=>{
+  localStorage.removeItem("userLog");
+  window.location="../pages/login.html";
+}
+const userLogin = JSON.parse(localStorage.getItem("userLog"))
+console.log(userLogin)
+document.getElementById("loginUser").innerText=`🚀${userLogin.nombre.toUpperCase()} ${userLogin.apellido.toUpperCase()}`
