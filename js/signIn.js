1038b693577554ed9c7880ca6143d2e1f7e75571
Ignacio Velazquez <ignaciov92@gmail.com>
2023-01-25 23:31:59 -0300

    empezando coonvenio

--- js/signIn.js
+++ js/signIn.js
@@ -40,39 +40,44 @@ const userSignIn = () => {
   const apellido = document.formSignIn.apellido.value.toLowerCase();
   const emailSignIn = document.formSignIn.emailSignIn.value.toLowerCase();
   const passSignIn = document.formSignIn.passSignIn.value
-  console.log(nombre,apellido,emailSignIn,passSignIn)
 
   const emailValidation=/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-
+  
   if (!emailValidation.test(emailSignIn)) {
     alert("el mail no tiene un formato correcto, vuelve a intentar")
     return
   } else {
-    let userFlag=false;
-    users.map(user=>{
-      if (user.email.toLowerCase() ===emailSignIn) {
-        userFlag=true;
-      }
-    })
-    if (userFlag) {
-      alert(
-        "El mail ingreso ya se encuentra registrado"
-      )
+    if (passSignIn=="" || nombre=="" || apellido=="" ) {
+      alert("Los campos deben estar completos")
       return
     } else {
-      alert("registro exitoso, ahora logueate")
+      let userFlag=false;
+      users.map(user=>{
+        if (user.email.toLowerCase() ===emailSignIn) {
+          userFlag=true;
+        }
+      })
+      if (userFlag) {
+        alert(
+          "El mail ingreso ya se encuentra registrado"
+        )
+        return
+      } else {
+        alert("registro exitoso, ahora logueate")
+        
+        users.push({
+          id: id+1,
+          nombre: nombre,
+          apellido: apellido,
+          email: emailSignIn,
+          password:passSignIn
+      })
+      console.log(users)
+      usersJson = JSON.stringify(users)
+      window.localStorage.setItem("users",usersJson)
+      window.location="../pages/login.html"
+      }
       
-      users.push({
-        id: id+1,
-        nombre: nombre,
-        apellido: apellido,
-        email: emailSignIn,
-        password:passSignIn
-    })
-    console.log(users)
-    usersJson = JSON.stringify(users)
-    window.localStorage.setItem("users",usersJson)
-    window.location="../pages/login.html"
     }
   }
 }
